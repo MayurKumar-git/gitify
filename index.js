7b68e3c883f599f55d8979139150a2b39222538d
Mayur Kumar <mayurkumar2704@gmail.com>
2022-04-13 19:05:04 +0530

    Made changes in index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log("Hello World");
 console.log("Hello world once again");
+console.log("Hello World");
+console.log("Hello world once again");
+console.log("Hello World");
+console.log("Hello world once again");
